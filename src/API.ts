f85e525cda317f61d11db60dcc3268d632c8a0b7
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2022-05-25 15:55:31 -0400

    create API req

--- src/API.ts
+++ src/API.ts
@@ -1,1 +1,36 @@
-import React from "react";
+import { shuffleArray } from "./utils";
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & { answers: string[] };
+
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+export const fetchQuizQuestions = async (
+  amount: number,
+  diffifculty: Difficulty
+) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${diffifculty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  console.log("dat: ", data);
+  // Below we are adding a new list with all the answers to make it easier
+  // to map over it in the UI
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
+};
