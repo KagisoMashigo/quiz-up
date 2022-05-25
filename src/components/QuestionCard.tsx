84f3c973018243a1712503e24dd8ccd885697698
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2022-05-25 13:45:16 -0400

    begin work on components

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {
+  question: string;
+  answer: string[];
+  callBack: any;
+  userAnswer: string;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard = () => <div>Question Card</div>;
+
+export default QuestionCard;
