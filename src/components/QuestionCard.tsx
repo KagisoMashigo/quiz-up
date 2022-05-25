4d80c897359564da295f307b874f971227bc264e
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2022-05-25 15:56:23 -0400

    finish card logic

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,14 +1,44 @@
 import React from "react";
+/// Types
+import { AnswerObject } from "../App";
+// Styles
 
 type Props = {
   question: string;
-  answer: string[];
-  callBack: any;
-  userAnswer: string;
+  answers: string[];
+  callBack: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
   questionNumber: number;
   totalQuestions: number;
 };
 
-const QuestionCard = () => <div>Question Card</div>;
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callBack,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => (
+  <div>
+    <p className="number">
+      Question: {questionNumber} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+        <div key={answer}>
+          <button
+            disabled={userAnswer ? true : false}
+            value={answer}
+            onClick={callBack}
+          >
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
 export default QuestionCard;
